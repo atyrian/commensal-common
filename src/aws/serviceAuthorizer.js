d619fb5131904a64d063041afddf2422bd09090b
Victor <victor.ricart@outlook.com>
2018-12-22 23:27:07 +0100

    Validation logic authorizer

--- src/aws/serviceAuthorizer.js
+++ src/aws/serviceAuthorizer.js
@@ -1,35 +1,37 @@
 const AWS = require('aws-sdk');
 const jwt = require('jsonwebtoken');
 const BaseAuthorizer = require('./baseAuthorizer');
+const { policyEffects, jwtOptions } = require('../constants/auth');
 
 class ServiceAuthorizer extends BaseAuthorizer {
   constructor(event) {
     super();
     this.event = event;
+    this.ssm = new AWS.SSM();
   }
 
-  authorize() {
-    return new Promise((resolve) => {
-      console.log('Running Service Authorizer');
-      const token = this.event.authorizationToken;
-      console.log('this was token:', token);
-      const response = super.generatePolicy('service', 'Allow');
-      console.log('super response:', response);
-      this.validateToken(token);
-      resolve(response);
-    });
+  async authorize() {
+    const policyEffect = await this.validateToken(this.event.authorizationToken);
+    console.log('policy effect was =>', policyEffect);
+    const policy = super.generatePolicy('service', policyEffect);
+    return policy;
   }
 
-  async validateToken(authorizationToken) {
-    this.token = authorizationToken.replace(/Bearer/g, '').trim();
-    const ssm = new AWS.SSM();
-    const cert = await ssm.getParameters({ Names: ['jwtRS256.key.pub'] }).promise();
-    console.log('cert was this =>', cert);
-    jwt.verify(this.token, cert, { algorithms: ['RS256'] }, (err, res) => {
-      if (err) {
-        console.log('Error decoding:', err);
-      }
-      console.log('response =>', res);
+  validateToken(authorizationToken) {
+    return new Promise(async (resolve) => {
+      this.token = authorizationToken.replace(/Bearer/g, '').trim();
+      const cert = await this.ssm.getParameters({ Names: ['jwtRS256.key.pub'] }).promise();
+      jwt.verify(this.token, cert.Parameters[0].Value, { algorithms: jwtOptions.allowedAlgorithms },
+        (err, res) => {
+          if (err) {
+            console.log('Validation Error:', err);
+            return resolve(policyEffects.deny);
+          }
+          if (res.aut === 'service') {
+            return resolve(policyEffects.allow);
+          }
+          return resolve(policyEffects.deny);
+        });
     });
   }
 }
