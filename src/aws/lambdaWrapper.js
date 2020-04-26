4250321899cbbe08454c47c33e42e778325137b4
Victor <victor.ricart@outlook.com>
2020-04-27 01:48:28 +0200

    Syntax

--- src/aws/lambdaWrapper.js
+++ src/aws/lambdaWrapper.js
@@ -3,20 +3,20 @@ const { HttpError } = require('../errors');
 function lambdaWrapper(fn) {
   return (event, context, callback) => Promise.resolve()
     .then(() => fn(event))
-    .then(response => callback(null, response)
-      .catch((error) => {
-        if (error instanceof HttpError) {
-          callback(null,
-            {
-              statusCode: 200,
-              body: JSON.stringify({ message: error.message, status: error.statusCode }),
-            });
-        } else {
-          console.log('Error not instance of HTTP Error');
-          console.log('Error:', error.stack);
-          callback(new Error('Internal Server Error'));
-        }
-      });
+    .then(response => callback(null, response))
+    .catch((error) => {
+      if (error instanceof HttpError) {
+        callback(null,
+          {
+            statusCode: 200,
+            body: JSON.stringify({ message: error.message, status: error.statusCode }),
+          });
+      } else {
+        console.log('Error not instance of HTTP Error');
+        console.log('Error:', error.stack);
+        callback(new Error('Internal Server Error'));
+      }
+    });
 }
 
 module.exports = lambdaWrapper;
